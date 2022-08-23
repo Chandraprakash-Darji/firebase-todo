6e9f057466aa2d0602fadf4217de7ea14ec0fb51
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-24 03:25:59 +0530

    added Store and UserContext Provider to App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+import { UserContext } from "../lib/context";
+import { useUserData } from "../lib/hooks";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    const user = useUserData();
+    return (
+        <Provider store={store}>
+            <UserContext.Provider value={user}>
+                <Component {...pageProps} />
+            </UserContext.Provider>
+        </Provider>
+    );
 }
 
-export default MyApp
+export default MyApp;
