5b9febc524ec56e02bb64beb505b2ac4fb9fa10f
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-24 03:11:42 +0530

    setup config of firebase

---
+++ lib/firebase.tsx
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_apiKey,
+    authDomain: process.env.NEXT_PUBLIC_authDomain,
+    projectId: process.env.NEXT_PUBLIC_projectId,
+    storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+    messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+    appId: process.env.NEXT_PUBLIC_appId,
+    measurementId: process.env.NEXT_PUBLIC_measurementId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+export const googleAuth = new GoogleAuthProvider();
+export const auth = getAuth();
+export const db = getFirestore(app);
