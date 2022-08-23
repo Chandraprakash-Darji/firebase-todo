09ffca8c01aba0790e87516002a66ea2ef43ef2a
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-24 04:08:14 +0530

    update: Logout Fun now reset the tasks

--- components/Logout.tsx
+++ components/Logout.tsx
@@ -1,11 +1,18 @@
+import { setTasks } from "../features/tasksSlice";
 import { auth } from "../lib/firebase";
+import { useAppDispatch } from "../lib/hooks";
 
 const Logout = () => {
+    const dispatch = useAppDispatch();
+    const handleLogout = () => {
+        auth.signOut();
+        dispatch(setTasks([]));
+    };
     return (
         <div>
             <button
                 className="flex relative group border bg-slate-50 text-slate-900 font-bold text-xl px-2 py-1 rounded-md"
-                onClick={() => auth.signOut()}
+                onClick={handleLogout}
             >
                 Log Out
             </button>
